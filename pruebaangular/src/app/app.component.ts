e580d49a621689b549cc6c4b0d69741abc934a72
Vaider13 <pabloquirco@hotmail.com>
2021-12-10 10:36:03 -0300

    Practicando Angular

--- pruebaangular/src/app/app.component.ts
+++ pruebaangular/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pruebaangular';
+  name: string;
+  email: string;
+  website: string;
+  talentos: string[];
+  switch: boolean;
+
+  constructor(){
+    this.name = "Taylorswift";
+    this.email = "taylorswift@gmail.com";
+    this.website = "http://www.taylorswift.com";
+    this.talentos =  ["Cantar", "Componer", "Bailar", "Correr", "Saltar"];
+    this.switch = false;
+  }
+
+  mostrarTalentos() {
+    this.switch = !this.switch;
+  }
 }
